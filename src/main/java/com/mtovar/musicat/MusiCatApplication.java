47ef2a99194c039b80ebce915f44383916c656b3
FMT86 <mtovar.francisco@gmail.com>
2025-05-29 14:48:27 +0200

    Initial Commit: Set up Spring Boot project with Gradle

---
+++ src/main/java/com/mtovar/musicat/MusiCatApplication.java
@@ -0,0 +1,13 @@
+package com.mtovar.musicat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusiCatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtovar/musicat/MusiCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtovar.musicat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusiCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
